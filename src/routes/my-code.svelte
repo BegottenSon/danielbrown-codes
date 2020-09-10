f547cb17d327c757e6fe139c2d6f0f9a743c578c
Begotten Son <BegottenSounds@DANIELs-MacBook-Pro.local>
2020-09-10 09:04:34 -0400

    added favicon and fixed codes page

--- src/routes/my-code.svelte
+++ src/routes/my-code.svelte
@@ -1,19 +1,16 @@
 <svelte:head>My Code</svelte:head>
 
 
-<p class="codepen" data-height="480" data-theme-id="dark" data-default-tab="js,result" data-user="BegottenSon" data-slug-hash="wvGaGGO" style="height: 485px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;" data-pen-title="Array Reduce Practice">
-    <span>See the Pen <a href="https://codepen.io/BegottenSon/pen/wvGaGGO">
-    Array Reduce Practice</a> by Daniel Brown (<a href="https://codepen.io/BegottenSon">@BegottenSon</a>)
-    on <a href="https://codepen.io">CodePen</a>.</span>
-</p>
+<iframe height="480" style="width: 100%;border-radius: 12px;" scrolling="no" title="Array Reduce Practice" src="https://codepen.io/BegottenSon/embed/wvGaGGO?height=441&theme-id=dark&default-tab=js,result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+    See the Pen <a href='https://codepen.io/BegottenSon/pen/wvGaGGO'>Array Reduce Practice</a> by Daniel Brown
+    (<a href='https://codepen.io/BegottenSon'>@BegottenSon</a>) on <a href='https://codepen.io'>CodePen</a>.
+  </iframe>
   
 
-<p class="codepen" data-height="480" data-theme-id="dark" data-default-tab="css,result" data-user="BegottenSon" data-slug-hash="OKbbEo" style="height: 480px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;" data-pen-title="Modal Learning">
-    <span>See the Pen <a href="https://codepen.io/BegottenSon/pen/OKbbEo">
-    Modal Learning</a> by Daniel Brown (<a href="https://codepen.io/BegottenSon">@BegottenSon</a>)
-    on <a href="https://codepen.io">CodePen</a>.</span>
-</p>
-<script async src="https://static.codepen.io/assets/embed/ei.js"></script>
+  <iframe height="480" style="width: 100%;border-radius: 12px;" scrolling="no" title="Modal Learning" src="https://codepen.io/BegottenSon/embed/OKbbEo?height=265&theme-id=dark&default-tab=css,result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+    See the Pen <a href='https://codepen.io/BegottenSon/pen/OKbbEo'>Modal Learning</a> by Daniel Brown
+    (<a href='https://codepen.io/BegottenSon'>@BegottenSon</a>) on <a href='https://codepen.io'>CodePen</a>.
+  </iframe>
 
 <style>
     .codepen {
