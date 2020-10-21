bde77b9141cffcaa2368bd51eabae025435df38b
Begotten Son <BegottenSounds@DANIELs-MacBook-Pro.local>
2020-10-20 22:00:52 -0400

    added crystals page

---
+++ src/routes/crystals.svelte
@@ -0,0 +1,102 @@
+<script>	
+	const crystals = [
+		{
+			name: "Amethyst",
+			image: "https://res.cloudinary.com/begottenson/image/upload/c_scale,w_400/v1603199254/Amethyst_tir0fc.jpg",
+			description: "Oh look, it's purple!",
+			bkgColor: " to bottom right, hsla(295, 82%, 19%, 1), hsla(342, 82%, 63%, 1)",
+			visible: false,
+		},
+		{
+			name: "Onyx",
+			image: "https://res.cloudinary.com/begottenson/image/upload/c_scale,w_400/v1603199255/onyx_ufkyq7.jpg",
+			description: "Oh look, it's black!",
+			bkgColor: " to bottom right, hsla(360, 82%, 12%, 1), hsla(200, 87%, 22%, 1)",
+			visible: false,
+		},
+		{
+			name: "Rose Quartz",
+			image: "https://res.cloudinary.com/begottenson/image/upload/c_scale,w_400/v1603199255/rose-quartz_i9biui.jpg",
+			description: "It's so pretty!",
+			bkgColor: " to bottom right, hsla(360, 96%, 46%, 1), hsla(312, 96%, 67%, 1)",
+			visible: false,
+		}
+	]
+</script>
+
+<section>
+    {#each crystals as crystal}
+    <div class="cell">
+        <div class="default" style="background-image: linear-gradient({crystal.bkgColor})" >
+            <input type=checkbox id="{crystal.name}" class="checkbox" bind:checked={crystal.visible}>
+            <label for="{crystal.name}" class="default-title">{crystal.name}</label>
+            <div class="card" 
+            class:expanded="{crystal.visible}"  
+            style="background-image: linear-gradient({crystal.bkgColor})" 
+            on:click="{() => crystal.visible = false}">
+                <img src={crystal.image} alt="{crystal.name}">
+                <h2>{crystal.name}</h2>
+                <p>{crystal.description}</p>
+            </div>
+        </div>
+    </div>
+	{/each}
+</section>
+
+<style>
+	section {
+		display: grid;
+		grid-template-columns: repeat( auto-fit, minmax(400px, 1fr));
+        height: 100%;
+	}
+
+    .cell {
+        width: 40vw;
+        height: 50vh;
+        position: relative;
+    }
+
+	.default {
+		display: grid;
+		/* position: absolute; */
+		height: 50vh;
+		place-items: center;
+		text-align: center;
+		color: whitesmoke;
+	}
+	
+	.card {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		/* position: absolute; */
+		top: 0;
+		left: 0;
+		width: 5vw;
+		height: 5vh;
+		filter: brightness(1.2);
+		opacity: 0;
+		cursor: pointer;
+		transition: 200ms ease-out;
+	}
+	
+	.default-title {
+		font-size: 2em;
+		background-color: #02535f;
+		border-radius: 8px;
+		padding: 1em;
+		cursor: pointer;
+	}
+	
+	.checkbox {
+		display: none;
+	}
+	
+	.expanded {
+        position: absolute;
+		height: 100vh;
+		width: 100vw;
+		opacity: 1;
+		z-index: 100;
+	}
+</style>
--- src/routes/my-code.svelte
+++ src/routes/my-code.svelte
@@ -1,5 +1,6 @@
 <script>
     import Transition from "../components/Transition.svelte"
+    import MusicAppIdea from "../components/MusicAppIdea.svelte";
 
 </script>
 <svelte:head>
@@ -8,17 +9,22 @@
 
 <Transition>
 
-<h2>Array Reduce Practice</h2>
-<iframe height="480" scrolling="no" title="Array Reduce Practice" src="https://codepen.io/BegottenSon/embed/wvGaGGO?height=441&theme-id=dark&default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
-    See the Pen <a href='https://codepen.io/BegottenSon/pen/wvGaGGO'>Array Reduce Practice</a> by Daniel Brown
-    (<a href='https://codepen.io/BegottenSon'>@BegottenSon</a>) on <a href='https://codepen.io'>CodePen</a>.
-</iframe>
-  
-<h2>Modal Practice</h2>
-<iframe height="480"  scrolling="no" title="Modal Learning" src="https://codepen.io/BegottenSon/embed/OKbbEo?height=265&theme-id=dark&default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
-    See the Pen <a href='https://codepen.io/BegottenSon/pen/OKbbEo'>Modal Learning</a> by Daniel Brown
-    (<a href='https://codepen.io/BegottenSon'>@BegottenSon</a>) on <a href='https://codepen.io'>CodePen</a>.
-</iframe>
+    <h2><a href="crystals">Crystals Grid</a></h2>
+
+    <h2>My Music App Idea</h2>
+    <MusicAppIdea />
+
+    <h2>Array Reduce Practice</h2>
+    <iframe height="480" scrolling="no" title="Array Reduce Practice" src="https://codepen.io/BegottenSon/embed/wvGaGGO?height=441&theme-id=dark&default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+        See the Pen <a href='https://codepen.io/BegottenSon/pen/wvGaGGO'>Array Reduce Practice</a> by Daniel Brown
+        (<a href='https://codepen.io/BegottenSon'>@BegottenSon</a>) on <a href='https://codepen.io'>CodePen</a>.
+    </iframe>
+    
+    <h2>Modal Practice</h2>
+    <iframe height="480"  scrolling="no" title="Modal Learning" src="https://codepen.io/BegottenSon/embed/OKbbEo?height=265&theme-id=dark&default-tab=result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+        See the Pen <a href='https://codepen.io/BegottenSon/pen/OKbbEo'>Modal Learning</a> by Daniel Brown
+        (<a href='https://codepen.io/BegottenSon'>@BegottenSon</a>) on <a href='https://codepen.io'>CodePen</a>.
+    </iframe>
 
 </Transition>
 
@@ -27,7 +33,9 @@
         width: 100%;
         margin-bottom: 3em;
     }
+
     h2 {
+        text-align: center;
         color: var(--blue);
     }
 </style>
