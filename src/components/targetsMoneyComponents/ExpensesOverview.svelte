1c536dd6986f6d85099d405b8c55189f2957f2a8
BegottenSon <danielbrown2d@gmail.com>
2021-01-20 09:41:05 -0500

    style expense section

--- src/components/targetsMoneyComponents/ExpensesOverview.svelte
+++ src/components/targetsMoneyComponents/ExpensesOverview.svelte
@@ -8,24 +8,46 @@
     main {
         background-color: var(--purple);
         border-radius: 8px;
-        box-shadow: 0px 0px 4px hsl(29, 49%, 82%);
+        box-shadow: 0px 0px 4px var(--wheat);
         text-align: center;
         width: 80vw;
+    }
+
+    h1 {
+        color: var(--wheat);
+        font-family: Arial, Helvetica, sans-serif;
+        margin: 0.5em;
+    }
+
+    .expenses-list {
+        display: flex;
+        flex-direction: column;
+        font-size: 0.8em;
+        margin: 1em 2em;
+    }
+
+    .list-item {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .money {
+        color: var(--off-yellow);
     }
 </style>
 
 <main>
     <h1>Monthly Expenses</h1>
     <section class="expenses-list">
-        <div class="liabilities">
+        <div class="liabilities list-item">
             <h3>Liabilities</h3>
             <h3 class="money">${liabilities}</h3>
         </div>
-        <div class="entertainment">
+        <div class="entertainment list-item">
             <h3>Entertainment</h3>
             <h3 class="money">${entertainment}</h3>
         </div>
-        <div class="personal">
+        <div class="personal list-item">
             <h3>Personal Care</h3>
             <h3 class="money">${personal}</h3>
         </div>
--- src/routes/targets-money-app.svelte
+++ src/routes/targets-money-app.svelte
@@ -16,6 +16,8 @@
         --purple: hsl(305, 89%, 15%);
         --dark-purple: hsl(305, 89%, 10%);
         --warm-blue: hsl(211, 81%, 15%);
+        --wheat: hsl(29, 49%, 82%);
+        --off-yellow: hsl(61, 60%, 73%);
         background-image: radial-gradient(var(--purple), var(--dark-purple));
         display: grid;
         gap: 2em;
